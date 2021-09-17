652530b5a5d646059b0113f6b9fea46d33274692
zhanghui <1579134335>
2021-09-17 10:04:35 +0800

    Initial commit

--- service-core/src/main/java/com/atguigu/srb/core/pojo/entity/IntegralGrade.java
+++ service-core/src/main/java/com/atguigu/srb/core/pojo/entity/IntegralGrade.java
@@ -1,16 +1,18 @@
 package com.atguigu.srb.core.pojo.entity;
 
-import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -40,10 +42,12 @@ public class IntegralGrade implements Serializable {
     @ApiModelProperty(value = "借款额度")
     private BigDecimal borrowAmount;
 
-    @ApiModelProperty(value = "创建时间",example = "2021-01-01 08:00:00")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
 
-    @ApiModelProperty(value = "更新时间",example = "2021-01-01 08:00:00")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @ApiModelProperty(value = "更新时间")
     private LocalDateTime updateTime;
 
     @ApiModelProperty(value = "逻辑删除(1:已删除，0:未删除)")

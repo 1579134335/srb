2704b920cbc66e72252bf29b9fc22108e25f7f95
zhanghui <1579134335>
2021-09-18 14:53:14 +0800

    Initial commit

--- service-core/src/main/java/com/atguigu/srb/core/controller/api/BorrowerController.java
+++ service-core/src/main/java/com/atguigu/srb/core/controller/api/BorrowerController.java
@@ -7,14 +7,18 @@ import com.atguigu.srb.core.service.BorrowerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-
+/**
+ * <p>
+ * 借款人 前端控制器
+ * </p>
+ *
+ * @author 张慧
+ * @since 2021-08-12
+ */
 @Api(tags = "借款人")
 @RestController
 @RequestMapping("/api/core/borrower")
@@ -30,5 +34,14 @@ public class BorrowerController {
         Long userId = JwtUtils.getUserId(token);
         borrowerService.saveBorrowerVOByUserId(borrowerVO, userId);
         return R.ok().message("信息提交成功");
+    }
+
+    @ApiOperation("获取借款人认证状态")
+    @GetMapping("/auth/getBorrowerStatus")
+    public R getBorrowerStatus(HttpServletRequest request){
+        String token = request.getHeader("token");
+        Long userId = JwtUtils.getUserId(token);
+        Integer status = borrowerService.getStatusByUserId(userId);
+        return R.ok().data("borrowerStatus", status);
     }
 }

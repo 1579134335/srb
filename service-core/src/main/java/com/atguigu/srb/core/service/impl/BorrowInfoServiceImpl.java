56393abecf3e1c54dbb3f1be58a2377eca516a46
zhanghui <1579134335>
2021-09-23 15:33:48 +0800

    Initial commit

--- service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowInfoServiceImpl.java
+++ service-core/src/main/java/com/atguigu/srb/core/service/impl/BorrowInfoServiceImpl.java
@@ -6,20 +6,28 @@ import com.atguigu.srb.core.enums.BorrowInfoStatusEnum;
 import com.atguigu.srb.core.enums.BorrowerStatusEnum;
 import com.atguigu.srb.core.enums.UserBindEnum;
 import com.atguigu.srb.core.mapper.BorrowInfoMapper;
+import com.atguigu.srb.core.mapper.BorrowerMapper;
 import com.atguigu.srb.core.mapper.IntegralGradeMapper;
 import com.atguigu.srb.core.mapper.UserInfoMapper;
 import com.atguigu.srb.core.pojo.entity.BorrowInfo;
+import com.atguigu.srb.core.pojo.entity.Borrower;
 import com.atguigu.srb.core.pojo.entity.IntegralGrade;
 import com.atguigu.srb.core.pojo.entity.UserInfo;
+import com.atguigu.srb.core.pojo.vo.BorrowInfoApprovalVO;
+import com.atguigu.srb.core.pojo.vo.BorrowerDetailVO;
 import com.atguigu.srb.core.service.BorrowInfoService;
+import com.atguigu.srb.core.service.BorrowerService;
 import com.atguigu.srb.core.service.DictService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -115,5 +123,55 @@ public class BorrowInfoServiceImpl extends ServiceImpl<BorrowInfoMapper, BorrowI
         });
 
         return borrowInfoList;
+    }
+
+
+    @Resource
+    private BorrowerMapper borrowerMapper;
+
+    @Resource
+    private BorrowerService borrowerService;
+
+    @Override
+    public Map<String, Object> getBorrowInfoDetail(Long id) {
+        //查询借款对象
+        BorrowInfo borrowInfo = baseMapper.selectById(id);
+        //组装对象
+        String returnMethod = dictService.getNameByParentDictCodeAndValue("returnMethod", borrowInfo.getReturnMethod());
+        String moneyUse = dictService.getNameByParentDictCodeAndValue("moneyUse", borrowInfo.getMoneyUse());
+        String status = BorrowInfoStatusEnum.getMsgByStatus(borrowInfo.getStatus());
+        borrowInfo.getParam().put("returnMethod",returnMethod);
+        borrowInfo.getParam().put("moneyUse",moneyUse);
+        borrowInfo.getParam().put("status",status);
+
+        //根据user_id获取借款人对象
+        QueryWrapper<Borrower> borrowerQueryWrapper = new QueryWrapper<>();
+        borrowerQueryWrapper.eq("user_id",borrowInfo.getUserId());
+        Borrower borrower = borrowerMapper.selectOne(borrowerQueryWrapper);
+
+        //组装借款人对象
+        BorrowerDetailVO borrowerDetailVO = borrowerService.getBorrowerDetailVOById(borrower.getId());
+
+        //组装数据
+        Map<String, Object> result = new HashMap<>();
+        result.put("borrowInfo",borrowInfo);
+        result.put("borrower",borrowerDetailVO);
+        return result;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void approval(BorrowInfoApprovalVO borrowInfoApprovalVO) {
+        //修改借款信息状态
+        Long borrowInfoId = borrowInfoApprovalVO.getId();
+        BorrowInfo borrowInfo = baseMapper.selectById(borrowInfoId);
+        borrowInfo.setStatus(borrowInfoApprovalVO.getStatus());
+        baseMapper.updateById(borrowInfo);
+
+        //审核通过则创建标的
+        if (borrowInfoApprovalVO.getStatus().intValue() == BorrowInfoStatusEnum.CHECK_OK.getStatus().intValue()){
+            //创建标的
+            //TODO
+        }
     }
 }
